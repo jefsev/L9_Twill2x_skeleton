50be42dbdbac42f8955a8a333c71967ba13b468b
jefsev <sevinga@protonmail.com>
2022-10-07 16:53:21 +0200

    fix: add fix to !empty logo

--- resources/views/site/partials/navigation.blade.php
+++ resources/views/site/partials/navigation.blade.php
@@ -1,17 +1,19 @@
 <nav id="navbar">
     <div class="navbar max-w-7xl mx-auto px-6 py-4 flex flex-row justify-between items-center">
         <a href="/" class="app-logo">
-            @if($menu->files('web_logo'))
+            @if(!empty($menu->file('web_logo')))
                 <img src="{{ $menu->file('web_logo') }}" alt="app logo">
             @else
                 Home
             @endif
         </a>
 
-        <div class="navigation flex flex-row items-center">
-            @foreach ($menu->menu_link as $link_attr)
-                <a href="{{ $link_attr['menu_item_slug'] }}" class="mx-4">{{ $link_attr['menu_item_title'] }}</a>
-            @endforeach
-        </div>
+        @if(!empty($menu->menu_link))
+            <div class="navigation flex flex-row items-center">
+                @foreach ($menu->menu_link as $link_attr)
+                    <a href="{{ $link_attr['menu_item_slug'] }}" class="mx-4">{{ $link_attr['menu_item_title'] }}</a>
+                @endforeach
+            </div>
+        @endif
     </div>
 </nav>
